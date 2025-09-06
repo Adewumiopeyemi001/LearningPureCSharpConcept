e1f3e3dc736c7fe1e5a2680098b0beccb32c0c76
user <user@DESKTOP-7Q3NR6R>
2025-09-06 12:48:05 -0700

    Add project files.

---
+++ Concepts/Introduction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace LearningPureCSharpConcept.Concepts
+{
+    public class Introduction
+    {
+        // Programming Language
+        // Introduction to C#
+
+        public string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0) return false;
+            }
+            return true;
+        }
+      
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using LearningPureCSharpConcept.Concepts;
+
+var result = new Introduction();
+
+
+var calculatePrimeNumber = result.IsPrime(6);
+
+Console.WriteLine(calculatePrimeNumber);
+
+
+Console.WriteLine("Hello, World!");
+
+
+
+
+
+
+
