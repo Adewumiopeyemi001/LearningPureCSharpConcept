a197b4367b46b2b2b26cc07a2d6cd10a39f342ad
Adewunmi001 <adewumiopeyemi001@yahoo.com>
2025-09-18 04:27:41 -0700

    Collections, GenericCollections

---
+++ Concepts/Collection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningPureCSharpConcept.Concepts
+{
+    internal class Collection
+    {
+        /*
+                Collection
+        Collection classes are specialized classes for data storage and retrieval
+        Allocates memory dynamically
+        All classes takes the data of object type
+        System.Collections namespace provides various classes providing different data structure
+
+        You can assign any value or any object type
+                Collection Classes
+        ArrayList : Get array and sort the element in particular order
+        Stack :
+        Queue :
+        Hashtable : having key and value
+        SordtedList
+
+
+        */
+    }
+}
---
+++ Concepts/GenericCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningPureCSharpConcept.Concepts
+{
+    internal class GenericCollection
+    {
+        /*
+                Generic Collection
+        Provides all the features similar to collections provided by System.Collections
+        Makes the collection type safe
+        System.Collections.Generic namespace provides several classes for stroing data
+                    Generic Collection Classes Classified as:
+        List : for array
+        Stack
+        Queue
+        Dictionary
+        SortedDictionary
+
+        */
+    }
+}
